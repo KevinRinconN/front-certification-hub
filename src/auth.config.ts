cf27bbd22489b92f8c9489b1cd0a4cc65b6a4a90
KevinRinconN <estebanrincon0926@gmail.com>
2024-04-06 00:11:02 -0500

    Add credentials validations and get user to  backend

---
+++ src/auth.config.ts
@@ -0,0 +1,57 @@
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { getMe, getToken } from "./service/auth/auth.service";
+
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: "/auth/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/admin");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/admin/home", nextUrl));
+      }
+      return true;
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.data = user;
+      }
+
+      return token;
+    },
+
+    session({ session, token, user }) {
+      session.user = token.data as any;
+      return session;
+    },
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        try {
+          if (!credentials || !credentials.username || !credentials.password) {
+            return null;
+          }
+
+          const username = credentials.username as string;
+          const password = credentials.password as string;
+
+          const { access_token } = await getToken(username, password);
+          const user = await getMe(access_token);
+
+          return { ...user, access_token };
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+};
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
